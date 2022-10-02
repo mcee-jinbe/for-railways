4f5aabc5d4027d86cb52dcb22a40f4f8e12658dd
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2022-10-02 11:33:22 +0900

    Update discordbot.js

--- discordbot.js
+++ discordbot.js
@@ -14,7 +14,7 @@ client.on("ready", () => {
   // ステータスを設定する
   setInterval(() => {
     client.user.setActivity({
-      name: `所属サーバー数は、${client.guilds.cache.size}サーバー｜　Ping値は、${client.ws.ping}ms｜　replit.comで起動中です`
+      name: `所属サーバー数は、${client.guilds.cache.size}サーバー｜　Ping値は、${client.ws.ping}ms｜　railway.appで起動中です`
     })
   }, 10000)
   client.channels.cache.get("913953017550745618").send("replit.comで起動しました！");
