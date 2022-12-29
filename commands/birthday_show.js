ecffcb14a3f8b4bb5a3ae656e48b034b5a92a66b
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2022-12-29 19:05:30 +0900

    いつものサーバー以外ではコマンドが動作しないように修正

--- commands/birthday_show.js
+++ commands/birthday_show.js
@@ -1,5 +1,5 @@
 const { ApplicationCommandOptionType } = require('discord.js');
-const { client } = require("../discordbot.js")
+const { client } = require("../index.js")
 
 module.exports = {
   data: {
@@ -27,54 +27,67 @@ module.exports = {
     ],
   },
   async execute(interaction) {
-    await interaction.deferReply({
-      ephemeral: true
-    });
-    const profileModel = require('../models/profileSchema.js');
-    let show_type = interaction.options.getString('type');
-    let show_user = interaction.options.getUser('user');
-
-    if (show_type == 'all') {
-      let database_members = await profileModel.find({});
-      let member_list = database_members.map(
-        (database_members) => database_members.user_name
-      );
-      await interaction.editReply({
-        embeds: [
-          {
-            title: '現在、データベースに登録されているユーザー一覧',
-            description:
-              `※誕生日が登録されていないユーザーも含みます。\n\`\`\`\n${member_list.join('\n')}\n\`\`\``,
-            color: 0xaad0ff,
-            timestamp: new Date(),
-          },
-        ],
-        ephemeral: false
+    if (interaction.guild.id == "768073209169444884") {
+      await interaction.deferReply({
+        ephemeral: true
       });
-    } else if (show_type == 'user') {
-      if (show_user !== null) {
-        const isBot = (await client.users.fetch(show_user)).bot;
-        if (!isBot) {
+      const profileModel = require('../models/profileSchema.js');
+      let show_type = interaction.options.getString('type');
+      let show_user = interaction.options.getUser('user');
 
-          let database_data = await profileModel.findById(show_user.id);
-          let database_month = database_data.birthday_month;
-          let database_day = database_data.birthday_day;
+      if (show_type == 'all') {
+        let database_members = await profileModel.find({});
+        let member_list = database_members.map(
+          (database_members) => database_members.user_name
+        );
+        await interaction.editReply({
+          embeds: [
+            {
+              title: '現在、データベースに登録されているユーザー一覧',
+              description:
+                `※誕生日が登録されていないユーザーも含みます。\n\`\`\`\n${member_list.join('\n')}\n\`\`\``,
+              color: 0xaad0ff,
+              timestamp: new Date(),
+            },
+          ],
+          ephemeral: false
+        });
+      } else if (show_type == 'user') {
+        if (show_user !== null) {
+          const isBot = (await client.users.fetch(show_user)).bot;
+          if (!isBot) {
 
-          if (database_month == 'no_data' || database_day == 'no_data') {
-            await interaction.editReply({
-              content: '誕生日が登録されていません。',
-              ephemeral: false
-            });
+            let database_data = await profileModel.findById(show_user.id);
+            let database_month = database_data.birthday_month;
+            let database_day = database_data.birthday_day;
+
+            if (database_month == 'no_data' || database_day == 'no_data') {
+              await interaction.editReply({
+                content: '誕生日が登録されていません。',
+                ephemeral: false
+              });
+            } else {
+              await interaction.editReply({
+                content: '',
+                embeds: [
+                  {
+                    title: `${show_user.username}さんの情報`,
+                    description: `ユーザー名：　\`${show_user.username}\`\nユーザーID：　\`${show_user.id}\`\n誕生日(登録されたもの)：　\`${database_month}月${database_day}日\``,
+                  },
+                ],
+                ephemeral: false
+              });
+            }
           } else {
             await interaction.editReply({
               content: '',
               embeds: [
                 {
-                  title: `${show_user.username}さんの情報`,
-                  description: `ユーザー名：　\`${show_user.username}\`\nユーザーID：　\`${show_user.id}\`\n誕生日(登録されたもの)：　\`${database_month}月${database_day}日\``,
-                },
-              ],
-              ephemeral: false
+                  title: 'エラー！',
+                  description: '指定された対象は「<:bot:1050345033305436170>」です。\n残念ながら、彼らに誕生日という概念を教えることが出来ません。対象には人を選んでください。',
+                  color: 0xFF0000
+                }
+              ]
             });
           }
         } else {
@@ -83,24 +96,24 @@ module.exports = {
             embeds: [
               {
                 title: 'エラー！',
-                description: '指定された対象は「<:bot:1050345033305436170>」です。\n残念ながら、彼らに誕生日という概念を教えることが出来ません。対象には人を選んでください。',
+                description: '誕生日の情報を表示する対象を指定してください。\n　例)　`/birthday_show [個人]　[@Hoshimikan6490]`',
                 color: 0xFF0000
               }
             ]
           });
         }
-      } else {
-        await interaction.editReply({
-          content: '',
-          embeds: [
-            {
-              title: 'エラー！',
-              description: '誕生日の情報を表示する対象を指定してください。\n　例)　`/birthday_show [個人]　[@Hoshimikan6490]`',
-              color: 0xFF0000
-            }
-          ]
-        });
       }
+    } else {
+      await interaction.reply({
+        embeds: [
+          {
+            title: 'エラー！',
+            description: `このサーバーでこのコマンドは実行できません。`,
+            color: 0xFF0000
+          }
+        ],
+        ephemeral: true
+      });
     }
   },
 };
